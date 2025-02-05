398921058bbfe8c4650ee35981183abf2605b638
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2025-02-06 02:24:09 +0530

    updated

--- run.py
+++ run.py
@@ -1,12 +1,6 @@
+from app import app  # Import the app from __init__.py
 import os
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Flask app is running!"
-
-PORT = int(os.environ.get("PORT", 5000))  # Get Railway-assigned port
-app.run(host="0.0.0.0", port=PORT, debug=True)
-
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 5000))  # Get Railway-assigned port
+    app.run(host="0.0.0.0", port=PORT, debug=True)
